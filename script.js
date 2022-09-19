d82e59c306481652f308ef777cb8e1a3b0339f62
GraydonvG <100849896+GraydonvG@users.noreply.github.com>
2022-09-19 16:29:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,47 +1,41 @@
-var button = document.getElementById("enter");
-var ul = document.querySelector("ul");
-var userinput = document.getElementById("userinput");
+const button = document.getElementById("enter");
+const ul = document.querySelector("ul");
+const userinput = document.getElementById("userinput");
 
-function inputLength() {
-  return userinput.value.length;
-}
+const inputLength = () => userinput.value.length;
 
-function createListElement() {
-  var li = document.createElement("li");
-  var textNode = document.createTextNode(userinput.value);
-  var span = document.createElement("span");
+const createListElement = () => {
+  const li = document.createElement("li");
+  const textNode = document.createTextNode(userinput.value);
+  const span = document.createElement("span");
   span.appendChild(textNode);
   li.appendChild(span);
   ul.appendChild(li);
   userinput.value = "";
   span.onclick = toggleDone;
 
-  var deleteBtn = document.createElement("button");
+  const deleteBtn = document.createElement("button");
   deleteBtn.setAttribute("class", "delete");
   deleteBtn.innerText = "Delete";
   li.appendChild(deleteBtn);
   deleteBtn.onclick = removeParent;
-}
+};
 
-function addItemBtn() {
+const addItemBtn = () => {
   if (inputLength() > 0) {
     createListElement();
   }
-}
+};
 
-function addItemKeypress(event) {
+const addItemKeypress = (event) => {
   if (inputLength() > 0 && event.which === 13) {
     createListElement();
   }
-}
+};
 
-function removeParent(eForEvent) {
-  eForEvent.target.parentNode.remove();
-}
+const removeParent = (eForEvent) => eForEvent.target.parentNode.remove();
 
-function toggleDone(anotherEvent) {
-  anotherEvent.target.classList.toggle("done");
-}
+const toggleDone = (anotherEvent) => anotherEvent.target.classList.toggle("done");
 
 button.addEventListener("click", addItemBtn);
 userinput.addEventListener("keypress", addItemKeypress);
