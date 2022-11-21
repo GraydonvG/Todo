2c70ca5f4ccf98e93515edd2ed1668a1477a8b0f
Graydon von Gossler <graydonvg@gmail.com>
2022-11-21 16:53:14 +0200

    cleaned up code

--- script.js
+++ script.js
@@ -1,41 +1,41 @@
-const button = document.getElementById("enter");
-const ul = document.querySelector("ul");
-const userinput = document.getElementById("userinput");
+const button = document.getElementById('enter');
+const ul = document.querySelector('ul');
+const userinput = document.getElementById('userinput');
 
 const inputLength = () => userinput.value.length;
 
-const createListElement = () => {
-  const li = document.createElement("li");
-  const textNode = document.createTextNode(userinput.value);
-  const span = document.createElement("span");
-  span.appendChild(textNode);
-  li.appendChild(span);
-  ul.appendChild(li);
-  userinput.value = "";
-  span.onclick = toggleDone;
-
-  const deleteBtn = document.createElement("button");
-  deleteBtn.setAttribute("class", "delete");
-  deleteBtn.innerText = "Delete";
-  li.appendChild(deleteBtn);
-  deleteBtn.onclick = removeParent;
-};
-
-const addItemBtn = () => {
-  if (inputLength() > 0) {
-    createListElement();
-  }
-};
-
-const addItemKeypress = (event) => {
-  if (inputLength() > 0 && event.which === 13) {
-    createListElement();
-  }
-};
+const toggleDone = (event) => event.target.classList.toggle('done');
 
 const removeParent = (event) => event.target.parentNode.remove();
 
-const toggleDone = (event) => event.target.classList.toggle("done");
+function createListElement() {
+  const li = document.createElement('li');
+  const textNode = document.createTextNode(userinput.value);
+  const span = document.createElement('span');
+  span.appendChild(textNode);
+  li.appendChild(span);
+  ul.appendChild(li);
+  userinput.value = '';
+  span.onclick = toggleDone;
 
-button.addEventListener("click", addItemBtn);
-userinput.addEventListener("keypress", addItemKeypress);
+  const deleteBtn = document.createElement('button');
+  deleteBtn.setAttribute('class', 'delete');
+  deleteBtn.innerText = 'Delete';
+  li.appendChild(deleteBtn);
+  deleteBtn.onclick = removeParent;
+}
+
+function addItemBtn() {
+  if (inputLength() > 0) {
+    createListElement();
+  }
+}
+
+function addItemKeypress(event) {
+  if (inputLength() > 0 && event.which === 13) {
+    createListElement();
+  }
+}
+
+button.addEventListener('click', addItemBtn);
+userinput.addEventListener('keypress', addItemKeypress);
